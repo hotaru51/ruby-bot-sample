0861d1ad61eda23b3a72f2ad9650c9dea2ef4f76
hotaru <36354847+hotaru51@users.noreply.github.com>
2019-09-23 23:16:29 +0900

    直接replyしなくても反応するように変更

--- ruby_bot_sample.rb
+++ ruby_bot_sample.rb
@@ -1,7 +1,7 @@
 require 'slack-ruby-bot'
 
 class RubyKurosawaBot < SlackRubyBot::Bot
-  command 'がんば' do |client, data, matach|
+  match /^.*(がんば|ふんば)$/ do |client, data, matach|
     client.say(text: 'ルビィ！', channel: data.channel)
   end
 end
